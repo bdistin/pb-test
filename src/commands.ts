b4c1850a9f9b0e01ee2b068de39539ad7cc452dd
bdistin <bdistin@gmail.com>
2020-05-25 20:18:57 -0500

    should make that equal

--- src/commands.ts
+++ src/commands.ts
@@ -16,7 +16,7 @@ export const commands: Record<string, (message: Message, args: string[]) => Prom
 		} catch (error) {
 			returned = error;
 		}
-		return message.channel.send(async (mb) => mb.setContent(`\`\`\`\n${inspect(returned, { depth: 0 })}\n\`\`\``), { prepend: '```\n', append: '```' });
+		return message.channel.send(async (mb) => mb.setContent(`\`\`\`\n${inspect(returned, { depth: 0 })}\n\`\`\``), { prepend: '```\n', append: '\n```' });
 	},
 
 	info(message: Message) {
